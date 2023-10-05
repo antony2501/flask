bd48128f390ce8c1086e668ee345bd73113fa22b
AnTony <tavanan47@gmail.com>
2023-10-05 20:11:09 +0700

    sap xong

--- app.py
+++ app.py
@@ -3,17 +3,27 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager, UserMixin, login_user, logout_user, login_required, current_user
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_admin import Admin
+from datetime import datetime
 from flask_admin.contrib.sqla import ModelView
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'your_secret_key'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:B20DCVT009@localhost/mydb'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
 login_manager = LoginManager(app)
 login_manager.login_view = 'login'
 admin = Admin(app, name='Admin Panel', template_mode='bootstrap3')
 
+# Định nghĩa lớp Python ánh xạ bảng "course"
+class Course(db.Model):
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    title = db.Column(db.String(255), nullable=False)
+    img = db.Column(db.String(255), nullable=False)
+    price = db.Column(db.Integer, nullable=False)
+    time_start = db.Column(db.Date, nullable=False)
+    student = db.Column(db.Integer, nullable=True)  # Giả sử student có thể là null
 
 class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -22,39 +32,46 @@ class User(UserMixin, db.Model):
     password = db.Column(db.String(120))
     role = db.Column(db.String(20))
 
+class CourseRegistration(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    course_id = db.Column(db.Integer, db.ForeignKey('course.id'), nullable=False)
+    registration_date = db.Column(db.DateTime, default=datetime.utcnow)
+
+    user = db.relationship('User', backref='registrations')
+    course = db.relationship('Course', backref='registrations')
+
+
 admin.add_view(ModelView(User, db.session)) 
+
+
+
 @login_manager.user_loader
 def load_user(user_id):
     return User.query.get(int(user_id))
 
+
+
 @app.route('/')
-def index():
-    return render_template('base.html')
+def home():
+    return render_template('home.html')
 
-@app.route('/login', methods=['GET', 'POST'])
-def login():
-    if request.method == 'POST':
-        email = request.form['email']
-        password = request.form['password']
-        user = User.query.filter_by(email=email).first()
-        if user and check_password_hash(user.password, password):
-            login_user(user)
-            if user.role == 'admin':
-                return redirect('/admin/')
-            else:
-                return redirect(url_for('user_profile'))
-        flash('Invalid email or password', 'error')
-    return render_template('login.html')
+@app.route('/bae1')
+def bae1():
+    return render_template('base1.html')
 
-@app.route('/admin')
-def admin_panel():
-    return redirect('/admin/')
+@app.route('/contact')
+def contact():
+    return render_template('contact.html')
 
-@app.route('/user_profile')
-@login_required
-def user_profile():
-    # Truy cập trang hồ sơ người dùng ở đây
-    return render_template('user_profile.html')
+
+@app.route('/course')
+def course():
+    courses = Course.query.all()
+    for course in courses:
+        print(f"Course ID: {course.id}, Title: {course.title}, Image: {course.img}, Price: {course.price}, Start Date: {course.time_start}, Student: {course.student}")
+        
+    return render_template('course.html', courses=courses)
 
 @app.route('/register', methods=['GET', 'POST'])
 def register():
@@ -70,9 +87,7 @@ def register():
             flash('Email already exists. Please use a different email.', 'error')
             return render_template('register.html')
         
-        # Tiến hành đăng ký
-        hashed_password = generate_password_hash(password, method='sha256')
-        new_user = User(name=name, email=email, password=hashed_password, role=role)
+        new_user = User(name=name, email=email, password=password, role=role)
         db.session.add(new_user)
         db.session.commit()
         flash('Registration successful. You can now log in.', 'success')
@@ -80,10 +95,58 @@ def register():
     
     return render_template('register.html')
 
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        email = request.form['email']
+        password = request.form['password']
+        user = User.query.filter_by(email=email).first()
+        if user and user.password == password:  # So sánh mật khẩu dưới dạng văn bản thô
+            login_user(user)
+            if user.role == 'admin':
+                return redirect('/admin/')
+            else:
+                return redirect(url_for('user_profile'))
+        flash('Invalid email or password', 'error')
+    return render_template('login.html')
+
+
+@app.route('/admin')
+def admin_panel():
+    return redirect('/admin/')
+
+@app.route('/user_profile')
+@login_required
+def user_profile():
+    # Truy cập trang hồ sơ người dùng ở đây
+    return render_template('user_profile.html')
+
+
 @app.route('/logout')
 @login_required
 def logout():
     logout_user()
     return redirect(url_for('login'))
+
+
+from datetime import datetime
+
+@app.route('/join_course/<int:course_id>', methods=['GET', 'POST'])
+
+@login_required
+def join_course(course_id):
+    course = Course.query.get(course_id)
+    if course:
+        registration = CourseRegistration(user_id=current_user.id, course_id=course_id)
+        db.session.add(registration)
+        db.session.commit()
+        flash('Successfully joined the course.', 'success')
+    else:
+        flash('Course not found.', 'error')
+    return redirect(url_for('courses'))
+
+
+
 if __name__ == '__main__':
+    db.create_all()
     app.run(debug=True)
--- data.sql
+++ data.sql
@@ -1,23 +1,41 @@
 CREATE DATABASE mydb;
 
-create table user {
+create table user (
     id int not null auto_increment primary key,
     name varchar(255) not null,
     email varchar(255) unique not null,
     password varchar(255) not null,
     role varchar(255) not null,
+)
+
+CREATE TABLE course(
+    id int not null auto_increment primary key,
+    title varchar(255) not null,
+    img varchar(255) not null,
+    price int not null,
+    time_start DATA NOT NULL,
+    student INT
+)
+
+class CourseRegistration(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    course_id = db.Column(db.Integer, db.ForeignKey('course.id'), nullable=False)
+    registration_date = db.Column(db.DateTime, default=datetime.utcnow)
+
+    user = db.relationship('User', backref='registrations')
+    course = db.relationship('Course', backref='registrations')
+
+
+create DATABASE video_course{
+    id int not null auto_increment primary key,
+    titles varchar(255),
+    link varchar(255),
+    course_id INT,
+    FOREIGN KEY ( course_id ) REFERENCES course(id)
 }
 
-CREATE TABLE score{
-    test1 int,
-    test2 int,
-    test3 int,
-    test4 int,
-    test5 int,
-    test6 int,
-    test7 int,
-    user_id int ,
-    FOREIGN KEY (user_id) REFERENCES users (id) 
-}
+
+
 
 
